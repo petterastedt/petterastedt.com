c77c9b959f2bb0ad24a3e984bb490c8cdcce8587
petterastedt <petter.astedt@gmail.com>
2020-01-30 22:12:09 +0100

    working on netlify lambda function

--- .netlify/getForm.js
+++ .netlify/getForm.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context, callback) => {
       body: JSON.stringify(body)
     })
   }
-
+  console.log(event.body)
   try {
     let response = await fetch('https://getform.io/f/fe0dc8a6-5e48-45f4-84c3-2f115ea31233', {
       method: event.httpMethod,
