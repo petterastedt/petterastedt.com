476f2658e36f647052005eadf6d7174c3cf6ec30
petterastedt <petter.astedt@gmail.com>
2020-02-01 13:39:13 +0100

    Setting up netlify lambda function

--- functions/getform.js
+++ functions/getform.js
@@ -1,7 +1,6 @@
 const axios = require("axios")
 
 exports.handler = (event, context, callback) => {
-  const {} = API_URL.process.env
   const pass = (body) => {
     callback(null, {
       statusCode: 200,
@@ -9,7 +8,7 @@ exports.handler = (event, context, callback) => {
     })
   }
 
-  axios("https://getform.io/f/fe0dc8a6-5e48-45f4-84c3-2f115ea31233", {
+  axios(API_URL.process.env, {
     method: event.httpMethod,
     data: event.body
   })
