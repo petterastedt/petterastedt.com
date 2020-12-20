86baa9d738438da4ac7f2fcf952925cbc09a4848
petterastedt <petter.astedt@gmail.com>
2020-12-20 23:58:01 +0100

    feat(Global): adjusting language settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import '../../node_modules/aos/dist/aos.css'
 
 export default ({data, location}) => {
   let lang
-  if (location) {
+  if (location.href) {
     lang = location.href.includes(".se") ? 0 : 1 // SET LANGUAGE (0 = SWEDISH, 1 = ENGLISH)
   } else {
     lang = 1
